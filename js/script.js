44248f10aaf84ddf98dc2cf468a93366c8e88c70
Diego Urbina <00544424@uca.edu.sv>
2024-09-18 22:28:10 -0600

    chore(styling): Add lyrics box

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-//WORKS
 const baseURL = 'https://api.lyrics.ovh/v1/'
-async function fetchLyrics() {
-    let response = await fetch('https://api.lyrics.ovh/v1/elton-john/tiny-dancer');
+let lyrics = ""
+
+async function fetchLyrics(artist, songName) {
+    let response = await fetch(`${baseURL}+${artist}/+${songName}`);
     if (response.ok) {
         const json = await response.json();
         console.log(json.lyrics);
     }
 }
 
-fetchLyrics();
+function formatSearch() {
+    return undefined;
+}
